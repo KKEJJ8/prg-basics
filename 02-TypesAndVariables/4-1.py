201524b3cd2b7127826d3095c8a247e87fba595f
Viktor Bazyuk <timmydeadinside@gmail.com>
2024-10-11 10:13:12 +0200

    11.10

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,8 @@
+###
+# Printing student's personal data
+#
+name = "Viktor"
+age = 17
+height = "186"
+age2 = age + 6
+print(f"My name is {name}, I am {age} years old, and my height is {height} cm. In 6 years I will be {age2} years old.")
